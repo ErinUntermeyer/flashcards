d6b5cfa249cfea226a2678f8dc2e9783253ae5b2
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-06-29 16:06:41 -0600

    Write test for each method in Turn class

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -26,4 +26,31 @@ describe('Turn', function () {
         expect(turn.card).to.equal(card);
     })
 
+    it('should return a guess', function () {
+        const turn = new Turn('object');
+        expect(turn.returnGuess()).to.equal('object');
+    })
+
+    it('should return a card', function () {
+        const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+        const turn = new Turn('object', card);
+        expect(turn.returnCard()).to.equal(card);
+    })
+
+    it('should evaluate the guess', function () {
+        const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+        const turn1 = new Turn('object', card);
+        const turn2 = new Turn('array', card);
+        expect(turn1.evaluateGuess()).to.equal(true);
+        expect(turn2.evaluateGuess()).to.equal(false);
+    })
+
+    it('should give feedback based on guess evaluation', function () {
+        const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+        const turn1 = new Turn('object', card);
+        const turn2 = new Turn('array', card);
+        expect(turn1.giveFeedback()).to.equal("correct!");
+        expect(turn2.giveFeedback()).to.equal("incorrect!");
+    })
+
 });
