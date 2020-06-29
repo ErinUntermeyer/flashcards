1312c618c61f72954f011047b48ee8183c2d6253
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-06-29 17:40:38 -0600

    Add Round class, add constructor, add methods

---
+++ src/Round.js
@@ -0,0 +1,35 @@
+const Turn = require("./Turn");
+
+class Round {
+    constructor(deck) {
+        this.deck = deck;
+        this.turns = 0;
+        this.incorrectGuesses = [];
+    }
+
+    returnCurrentCard() {
+        return this.deck[0];
+    }
+
+    takeTurn(guess) {
+        let card = this.returnCurrentCard();
+        let turn = new Turn(guess, card);
+        this.turns = this.turns + 1;
+        this.deck.shift();
+        if (turn.evaluateGuess() === false) {
+            this.incorrectGuesses.push(card.id);
+        }
+        return turn.giveFeedback();
+    }
+
+    calculatePercentCorrect() {
+        return (this.turns - this.incorrectGuesses.length/this.turns * 100);
+    }
+
+    endRound() {
+        console.log(`** Round over! ** You answered ${this.calculatePercentCorrect}% of the questions correctly!`)
+        return process.exit()
+    }
+}
+
+module.exports = Round;
