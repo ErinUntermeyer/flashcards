e3f861d683e8e609b179ca4717772be94418834c
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-06-30 15:28:05 -0600

    Update constructor to track currentRound, add start method to begin a game

--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,23 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Deck = require('./Deck');
+const Round = require('./Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = {};
+  }
 
-  printMessage(deck, round) {
+  start() {
+    let cards = prototypeQuestions;
+    let deck = new Deck(cards);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  }
+
+  printMessage(deck) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
