52d2bd1e07465537f688217a1b8aa3d6c660e45f
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-07-02 12:46:51 -0600

    Add message to endRound method to play again if score 90 or less

--- src/Round.js
+++ src/Round.js
@@ -27,8 +27,13 @@ class Round {
   }
 
   endRound() {
-    console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`)
-    return process.exit()
+    if (this.calculatePercentCorrect() <= 90) {
+      console.log(`Oops! You scored a ${this.calculatePercentCorrect()}%! Play again to score 90% or higher!`);
+      return process.exit();
+    } else {
+    console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`);
+    return process.exit();
+    }
   }
 }
 
